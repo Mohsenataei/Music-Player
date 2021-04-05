71bb4c5a958ed154541a2294e59b4dd18212ab82
mohsen.ataei <mohsenataeiasad@gmail.com>
2021-04-05 15:04:54 +0430

    Add kotlin coroutines

--- app/src/main/java/io/github/maa96/musicplayer/MainActivity.kt
+++ app/src/main/java/io/github/maa96/musicplayer/MainActivity.kt
@@ -1,11 +1,20 @@
 package io.github.maa96.musicplayer
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
+
+
